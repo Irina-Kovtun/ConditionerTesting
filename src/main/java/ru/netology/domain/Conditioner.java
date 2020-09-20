5fcdc03a6b8d12dd51a229b553883d64dc633db6
Irina-Kovtun <irinakovtun.us@gmail.com>
2020-09-20 19:08:01 +0300

    Tests are added

--- src/main/java/ru/netology/domain/Conditioner.java
+++ src/main/java/ru/netology/domain/Conditioner.java
@@ -1,9 +1,94 @@
+//на всякий случай класс Conditioner
+// package ru.netology.domain;
+//
+//public class Conditioner {
+//    String name;
+//    int maxTemperature;
+//    int minTemperature;
+//    int currentTemperature;
+//    boolean on;
+//}
+
 package ru.netology.domain;
 
 public class Conditioner {
-    String name;
-    int maxTemperature;
-    int minTemperature;
-    int currentTemperature;
-    boolean on;
+    private String name;
+    private int maxTemperature = 30;
+    private int minTemperature = 10;
+    private int currentTemperature = (maxTemperature + minTemperature) / 2;
+    private boolean on;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public int getMaxTemperature() {
+
+        return maxTemperature;
+    }
+
+    public void setMaxTemperature(int maxTemperature) {
+
+        this.maxTemperature = maxTemperature;
+    }
+
+    public int getMinTemperature() {
+
+        return minTemperature;
+    }
+
+    public void setMinTemperature(int minTemperature) {
+
+        this.minTemperature = minTemperature;
+    }
+
+    public int getCurrentTemperature() {
+
+        return currentTemperature;
+    }
+
+    public void setCurrentTemperature(int currentTemperature) {
+        if (currentTemperature > maxTemperature) {
+            return;
+        }
+        if (currentTemperature < minTemperature) {
+            return;
+        }
+        this.currentTemperature = currentTemperature;
+//        альтернативный вариант кода для данного метода:
+//        if(currentTemperature <= maxTemperature) {
+//            if(currentTemperature >= minTemperature) {
+//                this.currentTemperature = currentTemperature;
+//            }
+//        }
+    }
+
+    public void increaseCurrentTemperature() {
+        if (currentTemperature == maxTemperature) {
+            return;
+        }
+        currentTemperature++;
+    }
+
+    public void decreaseCurrentTemperature() {
+        if (currentTemperature == minTemperature) {
+            return;
+        }
+        currentTemperature--;
+      }
+
+    public boolean isOn() {
+
+        return on;
+    }
+
+    public void setOn(boolean on) {
+
+        this.on = on;
+    }
 }
--- src/test/java/ru/netology/domain/ConditionerTest.java
+++ src/test/java/ru/netology/domain/ConditionerTest.java
@@ -1,22 +1,59 @@
 package ru.netology.domain;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ConditionerTest {
-    
+
+//    @Test
+//    public void shouldCreate() {
+//        Conditioner conditioner = new Conditioner();
+//        assertEquals(20, conditioner.getCurrentTemperature());
+//        assertEquals(10, conditioner.getMinTemperature());
+//        assertEquals(30, conditioner.getMaxTemperature());
+//        assertNull(conditioner.getName());
+//        assertFalse(conditioner.isOn());
+//    }
+
     @Test
-    public void shouldCreate() {
+     void shouldIncreaseCurrentTemperature() {
         Conditioner conditioner = new Conditioner();
-        assertEquals(0, conditioner.currentTemperature);
-        assertEquals(0, conditioner.minTemperature);
-        assertEquals(0, conditioner.maxTemperature);
-        assertNull(conditioner.name);
-        assertFalse(conditioner.on);
-
-        System.out.println("Hi!");
-
+        conditioner.setCurrentTemperature(20);
+        conditioner.increaseCurrentTemperature();
+        int expected = 21;
+        int actual = conditioner.getCurrentTemperature();
+        assertEquals(expected, actual);
     }
 
+    @Test
+    void shouldNotIncreaseCurrentTemperature() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setCurrentTemperature(30);
+        conditioner.increaseCurrentTemperature();
+        int expected = 30;
+        int actual = conditioner.getCurrentTemperature();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldDecreaseCurrentTemperature() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setCurrentTemperature(25);
+        conditioner.decreaseCurrentTemperature();
+        int expected = 24;
+        int actual = conditioner.getCurrentTemperature();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldNotDecreaseCurrentTemperature() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setCurrentTemperature(10);
+        conditioner.decreaseCurrentTemperature();
+        int expected = 10;
+        int actual = conditioner.getCurrentTemperature();
+        assertEquals(expected, actual);
+    }
 }
